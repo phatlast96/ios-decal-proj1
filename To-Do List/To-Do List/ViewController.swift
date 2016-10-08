e07b81e12e60f10ada822f2bdfa398bc83b3ddd8
Phat Pham <phatpham@berkeley.edu>
2016-10-08 13:38:58 -0700

    Starting the project

---
+++ To-Do List/To-Do List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Phat Pham on 10/8/16.
+//  Copyright © 2016 Phat Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
